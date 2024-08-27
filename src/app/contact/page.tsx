2593c187932d623f706857c8c9ac79bf02ea3aea
Diego Monte Alto <diego@diegoalto.co.uk>
2024-08-27 08:05:17 +0100

    hello

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function ContactPage() {
+  return (
+    <main>
+      <h1>Contact</h1>
+      <p>
+        Send me an email at{" "}
+        <a href="mailto:diego@diegoalto.co.uk">diego@diegoalto.co.uk</a>
+      </p>
+    </main>
+  );
+}
