86d54d0eef997da5f03efcc546083ec85441f308
Diego Monte Alto <diego@diegoalto.co.uk>
2024-08-27 10:02:51 +0100

    test cicd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div role="heading" aria-level={1}>Olá</div>
+      <div role="heading" aria-level={1}>Olá!</div>
     </main>
   )
 }
