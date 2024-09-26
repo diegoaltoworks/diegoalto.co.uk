e439bb9d7d522c09e8c382ac859bea939a7b6b89
Diego Alto <diego@diegoalto.co.uk>
2024-09-26 07:34:42 +0100

    dummy 3rd slide chat link

--- src/components/Template/Homepage/Slide3.tsx
+++ src/components/Template/Homepage/Slide3.tsx
@@ -1,28 +1,34 @@
 import Link from "next/link";
 import styles from "./Slides.module.scss";
-import Icons from "@/components/Template/Icons";
+import { Tooltip } from "@mui/material";
 
 export default function Slide3() {
 	return (
 		<div className={styles.slide} data-testid="homepage-slide" data-slide="3">
 			<div role="heading" aria-level={1}>
-				You can <Link href={"/contact"}>contact me here</Link>,
+				You can <Link href={"/contact"}>contact me here</Link>.
 			</div>
 			<div role="subheading" aria-level={2}>
-				<Link href="/register">register</Link>/<Link href="/login">login</Link>{" "}
-				to
+				Or <Link href="/register">register</Link>/
+				<Link href="/login">login</Link> to
 			</div>
 			<div role="subheading" aria-level={2}>
-				chat <Link href="/chat">chat</Link> with my AI bot
+				<Tooltip title="Under construction">
+					<span>
+						<Link href="#">chat</Link> with my AI bot
+					</span>
+				</Tooltip>
 			</div>
 			<div role="subheading" aria-level={3}>
 				(also on{" "}
 				<Link href="https://t.me/DiegoAlto">
-					<Icons.TelegramIcon /> Telegram
-				</Link>
-				or
+					{/* <Icons.TelegramIcon fontSize="small" />  */}
+					Telegram
+				</Link>{" "}
+				or{" "}
 				<Link href="https://wa.me/447467053623">
-					<Icons.WhatsappIcon /> Whatsapp
+					{/* <Icons.WhatsappIcon fontSize="small" />  */}
+					Whatsapp
 				</Link>
 				)
 			</div>
