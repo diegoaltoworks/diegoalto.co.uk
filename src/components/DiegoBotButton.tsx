60d043a6b642270059b713b463ab5f0b10e4fd66
Diego <diego@diegoalto.co.uk>
2025-11-09 15:09:01 +0000

    feat: upgraded chat integrtion

--- src/components/DiegoBotButton.tsx
+++ src/components/DiegoBotButton.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { useEffect, useRef } from "react";
+
 import type { ChatButtonInstance } from "@/types/fyne";
 
 export const DiegoBotButton: React.FC = () => {
--- src/components/DiegoBotChat.tsx
+++ src/components/DiegoBotChat.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { useEffect, useRef } from "react";
+
 import { Box } from "@mui/material";
 import type { ChatInstance } from "@/types/fyne";
 
